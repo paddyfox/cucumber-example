2f98fbc80b79499630588911b92f7b25b415242d
Paddy Fox <patrick.fox@digital.homeoffice.gov.uk>
2022-06-22 11:08:17 +0100

    Add ability to accept cookies on Amazon

--- src/main/java/uk/kainos/seleniumframework/site/pageobjects/AmazonHomePage.java
+++ src/main/java/uk/kainos/seleniumframework/site/pageobjects/AmazonHomePage.java
@@ -11,9 +11,12 @@ import java.util.stream.Collectors;
 public class AmazonHomePage extends Site {
 
     @FindBy(id = "nav-logo-sprites") private WebElement pageHeaderLogo;
+    @FindBy(id = "sp-cc-accept") private WebElement acceptCookies;
     @FindBy(id = "twotabsearchtextbox") private WebElement searchBar;
     @FindBy(css = "#nav-search-submit-text > input") private WebElement searchButton;
     @FindBy(tagName = "h2") private List<WebElement> searchResults;
+    @FindBy(xpath = "//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div[2]/div/div/div/div/div/div[2]/div/div/div[1]/h2/a") private WebElement snoopDogCDResult;
+    @FindBy(xpath = "//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div[2]/div/div/div/div/div/div[2]/div/div/div[1]/h2/a") private WebElement echoDotResult;
 
     public void amazonWebsiteIsDisplayed() {
         pageHeaderLogo.isDisplayed();
@@ -27,9 +30,14 @@ public class AmazonHomePage extends Site {
         searchButton.click();
     }
 
-    public void selectFirstSearchResult() {
-        //TODO: Make this work
-        searchResults.get(0).click();
+    public void acceptCookies() { acceptCookies.click(); }
+
+    public void selectFirstResultForSnoopDogg() {
+        snoopDogCDResult.click();
+    }
+
+    public void selectFirstResultForEchoDot() {
+        echoDotResult.click();
     }
 
     public List<String> getAllSearchResults() {
